0e786ee8db97d4309c936960d45bee5297820ee5
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2021-11-05 14:32:37 -0700

    iterations

--- EOMM/EOMM/Program.cs
+++ EOMM/EOMM/Program.cs
@@ -1,18 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System;
+﻿using System;
 
 namespace EOMM {
   public static class Program {
     public static void Main(string[] args) {
       Console.WriteLine("Hello, World!");
 
+      for (var i = 0; i < 100; i++) {
+        Simulate(i);
+      }
+    }
+
+    private static void Simulate(int iteration = 0) {
       var graph = new PlayerGraph();
 
       // Retained players with random algorithm
       var retainedPlayers = new MatchSimulator(100, graph).Run(new RandomMatchmaking());
 
-      Console.WriteLine($"Retained players: {retainedPlayers}");
+      Console.WriteLine($"{iteration.ToString().PadRight(5)} | Retained players: {retainedPlayers}");
     }
   }
 }
